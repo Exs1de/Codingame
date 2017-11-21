e54ad56adc4be355638b870e17b2b922484764e4
Alex <thewellfox@gmail.com>
2017-11-21 18:41:20 +0300

    Update Mars lander - episode 1.cpp

--- EASY/Mars lander - episode 1.cpp
+++ EASY/Mars lander - episode 1.cpp
@@ -1,47 +1,41 @@
-using System;
-using System.Linq;
-using System.IO;
-using System.Text;
-using System.Collections;
-using System.Collections.Generic;
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
 
 /**
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
- 
-class Player
+int main()
 {
-    static void Main(string[] args)
-    {
-        string[] inputs;
-        int surfaceN = int.Parse(Console.ReadLine()); // the number of points used to draw the surface of Mars.
-        for (int i = 0; i < surfaceN; i++)
-        {
-            inputs = Console.ReadLine().Split(' ');
-            int landX = int.Parse(inputs[0]); // X coordinate of a surface point. (0 to 6999)
-            int landY = int.Parse(inputs[1]); // Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
-        }
-
-        // game loop
-        while (true)
-        {
-            inputs = Console.ReadLine().Split(' ');
-            int X = int.Parse(inputs[0]);
-            int Y = int.Parse(inputs[1]);
-            int hSpeed = int.Parse(inputs[2]); // the horizontal speed (in m/s), can be negative.
-            int vSpeed = int.Parse(inputs[3]); // the vertical speed (in m/s), can be negative.
-            int fuel = int.Parse(inputs[4]); // the quantity of remaining fuel in liters.
-            int rotate = int.Parse(inputs[5]); // the rotation angle in degrees (-90 to 90).
-            int power = int.Parse(inputs[6]); // the thrust power (0 to 4).
-
-            // Write an action using Console.WriteLine()
-            // To debug: Console.Error.WriteLine("Debug messages...");
-                
-            if(vSpeed < -44)power = 4;
-            
-            // 2 integers: rotate power. rotate is the desired rotation angle (should be 0 for level 1), power is the desired thrust power (0 to 4).
-            Console.WriteLine("0 {0}", power);
-        }
+    int surfaceN; // the number of points used to draw the surface of Mars.
+    cin >> surfaceN; cin.ignore();
+    for (int i = 0; i < surfaceN; i++) {
+        int landX; // X coordinate of a surface point. (0 to 6999)
+        int landY; // Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
+        cin >> landX >> landY; cin.ignore();
     }
+
+    // game loop
+    while (1) {
+        int X;
+        int Y;
+        int hSpeed; // the horizontal speed (in m/s), can be negative.
+        int vSpeed; // the vertical speed (in m/s), can be negative.
+        int fuel; // the quantity of remaining fuel in liters.
+        int rotate; // the rotation angle in degrees (-90 to 90).
+        int power; // the thrust power (0 to 4).
+        cin >> X >> Y >> hSpeed >> vSpeed >> fuel >> rotate >> power; cin.ignore();
+        
+        // Write an action using cout. DON'T FORGET THE "<< endl"
+        // To debug: cerr << "Debug messages..." << endl;
+
+        if(vSpeed < -44)power = 4;
+       
+        // 2 integers: rotate power. rotate is the desired rotation angle (should be 0 for level 1), power is the desired thrust power (0 to 4).
+        cout << 0 << " " << power << endl;
+    } 
 }
