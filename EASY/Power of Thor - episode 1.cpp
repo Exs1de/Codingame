30c2764c99d80fa075801b1ef7b690f7c8354139
Alex <thewellfox@gmail.com>
2017-11-21 18:40:32 +0300

    Update Power of Thor - episode 1.cpp

--- EASY/Power of Thor - episode 1.cpp
+++ EASY/Power of Thor - episode 1.cpp
@@ -1,9 +1,9 @@
-using System;
-using System.Linq;
-using System.IO;
-using System.Text;
-using System.Collections;
-using System.Collections.Generic;
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
 
 /**
  * Auto-generated code below aims at helping you parse
@@ -11,54 +11,52 @@ using System.Collections.Generic;
  * ---
  * Hint: You can use the debug stream to print initialTX and initialTY, if Thor seems not follow your orders.
  **/
- 
-class Player
+int main()
 {
-    static void Main(string[] args)
-    {
-        string[] inputs = Console.ReadLine().Split(' ');
-        int lightX = int.Parse(inputs[0]); // the X position of the light of power
-        int lightY = int.Parse(inputs[1]); // the Y position of the light of power
-        int initialTX = int.Parse(inputs[2]); // Thor's starting X position
-        int initialTY = int.Parse(inputs[3]); // Thor's starting Y position
-        
-        int TX = initialTX;
-        int TY = initialTY;
-        int X = lightX;
-        int Y = lightY;
+    int lightX; // the X position of the light of power
+    int lightY; // the Y position of the light of power
+    int initialTX; // Thor's starting X position
+    int initialTY; // Thor's starting Y position
+    cin >> lightX >> lightY >> initialTX >> initialTY; cin.ignore();
 
-        string dir;
-        bool set_dir = false;
-        
-        // game loop
-        while (true)
-        {
-            int remainingTurns = int.Parse(Console.ReadLine()); // The remaining amount of turns Thor can move. Do not remove this line.
-
-            // Write an action using Console.WriteLine()
-            // To debug: Console.Error.WriteLine("Debug messages...");
-
-            dir = "";
-            if(TY < Y){dir = "S"; TY++; set_dir = true;}
-            if(TY > Y){dir = "N"; TY--; set_dir = true;}
-            if(TX < X){dir += "E"; TX++; set_dir = true;}
-            if(TX > X){dir += "W"; TX--; set_dir = true;}
+    int TX = initialTX;
+    int TY = initialTY;
+    int X = lightX;
+    int Y = lightY;
     
-            if(!set_dir){
-                if(TY == Y){
-                    if(TX < X){dir = "E"; TX++;}
-                    if(TX > X){dir = "W"; TX--;}
-                } 
-                
-                if(TX == X){
-                    if(TY < Y){dir = "S"; TY++;}
-                    if(TY > Y){dir = "N"; TY--;}  
-                }
-            }
-            set_dir = false;
+    string dir;
+    bool set_dir = false;
+    
+    // game loop
+    while (1) {
+        int remainingTurns; // The remaining amount of turns Thor can move. Do not remove this line.
+        cin >> remainingTurns; cin.ignore();
 
-            // A single line providing the move to be made: N NE E SE S SW W or NW
-            Console.WriteLine(dir);
+        // Write an action using cout. DON'T FORGET THE "<< endl"
+        // To debug: cerr << "Debug messages..." << endl;
+        dir = "";
+        if(TY < Y){dir = "S"; TY++; set_dir = true;}
+        if(TY > Y){dir = "N"; TY--; set_dir = true;}
+        if(TX < X){dir += "E"; TX++; set_dir = true;}
+        if(TX > X){dir += "W"; TX--; set_dir = true;}
+
+        if(!set_dir){
+            if(TY == Y){
+                if(TX < X){dir = "E"; TX++;}
+                if(TX > X){dir = "W"; TX--;}
+            } 
+            
+            if(TX == X){
+                if(TY < Y){dir = "S"; TY++;}
+                if(TY > Y){dir = "N"; TY--;}  
+            }
         }
+        set_dir = false;
+            
+        cerr << TY << endl; 
+        cout << dir << endl;
+        
+        
+        
     }
 }
